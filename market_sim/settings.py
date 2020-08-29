38591fc0995b16a8695636b2b66b31ab4fdfb3c8
vibhumahendru <vibhumahendru@gmail.com>
2020-08-30 00:02:16 +0530

    more

--- market_sim/settings.py
+++ market_sim/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'c))*ibzwh$xla1rn17-^irp(kcbwupze-k#ih6x6n_7^$+7l7h'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
