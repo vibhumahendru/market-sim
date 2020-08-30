5c7938307bc3b0be5d95a120b18d955224f754c9
vibhumahendru <vibhumahendru@gmail.com>
2020-08-30 10:54:35 +0530

    dummy

--- market_sim/urls.py
+++ market_sim/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from django.conf.urls import url, include
 from market_core.views import TradeViewSet, ComputationViewset
 from rest_framework.routers import DefaultRouter
-
+ 
 trade_router = DefaultRouter()
 trade_router.register(r'', TradeViewSet, basename='trade')
 
